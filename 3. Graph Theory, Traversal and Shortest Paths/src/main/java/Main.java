6f141ff07e9cdd19b516f92210362e4b7f311538
Galin Manov <thenightarchervclaymore@gmail.com>
2022-09-01 22:41:13 +0300

    Init

---
+++ 3. Graph Theory, Traversal and Shortest Paths/src/main/java/Main.java
@@ -0,0 +1,85 @@
+package main.java;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+
+            if (input.trim().equals("")) {
+                graph.add(new ArrayList<>());
+            } else {
+                List<Integer> elements = Arrays.stream(input.split("\\s+")).
+                        map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+                graph.add(elements);
+            }
+        }
+
+        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
+
+        for (Deque<Integer> connectedComponent : connectedComponents) {
+            System.out.print("Connected component: ");
+            for (Integer integer : connectedComponent) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size()];
+        List<Deque<Integer>> components = new ArrayList<>();
+
+        for (int start = 0; start < graph.size(); start++) {
+            if (!visited[start]) {
+                components.add(new ArrayDeque<>());
+                bfs(start, components, graph, visited);
+            }
+        }
+
+        return components;
+    }
+
+    private static void bfs(int start, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        visited[start] = true;
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+
+            components.get(components.size() - 1).offer(node);
+
+            for (int child : graph.get(node)) {
+                if (!visited[child]){
+                    visited[child] =true;
+                    queue.offer(child);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int start, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        if (!visited[start]) {
+            visited[start] = true;
+            for (int child : graph.get(start)) {
+                dfs(child, components, graph, visited);
+            }
+            components.get(components.size() - 1).offer(start);
+        }
+    }
+
+    public static Collection<String> topSort(Map<String, List<String>> graph) {
+        throw new AssertionError("Not Implemented");
+    }
+}
